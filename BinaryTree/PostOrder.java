a12132cf7ad6728c6e0455c7e7fabca14496ea04
Ela <androelam@gmail.com>
2022-06-09 23:07:20 +0530

    PostOrder

---
+++ BinaryTree/PostOrder.java
@@ -0,0 +1,35 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+       List<Integer> result = new ArrayList<>();
+       postorderTraversal(root, result);
+       return result;
+   }
+   private void postorderTraversal(TreeNode root, List<Integer> ds) {
+       if(root == null) {
+           return;
+       }
+       
+      
+       postorderTraversal(root.left, ds);
+       postorderTraversal(root.right, ds);
+       ds.add(root.val);
+   }
+}
