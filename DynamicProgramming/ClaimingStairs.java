4f11f7f845a887da60db2fe4263512ceaab3caff
Ela <androelam@gmail.com>
2022-06-12 00:50:55 +0530

    claiming stairs

---
+++ DynamicProgramming/ClaimingStairs.java
@@ -0,0 +1,36 @@
+package DynamicProgramming;
+
+
+/*
+ * 70. Climbing Stairs
+Easy
+
+12138
+
+371
+
+Add to List
+
+Share
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ https://www.youtube.com/watch?v=Y0lT9Fck7qI
+ */
+public class ClaimingStairs {
+    public static void main(String[] args) {
+        System.out.println(maxStairsClaim(5));
+    }
+
+    private static int maxStairsClaim(int n) {
+        int prev = 1 , prev1 = 1;
+        for(int i = n-2; i >= 0; i--) {
+            int temp = prev + prev1;
+            prev1 = prev;
+            prev = temp;
+        }
+        return prev;
+    }
+    
+}
