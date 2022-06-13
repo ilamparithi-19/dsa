f042ab4156896d80d82eaf751c9b1b41685d4e1a
Ela <androelam@gmail.com>
2022-06-13 23:58:03 +0530

    longest onsec

--- Arrays/LongestConsecutive.java
+++ Arrays/LongestConsecutive.java
@@ -1,7 +1,14 @@
 package Arrays;
 
-class Solution {
-    public int longestConsecutive(int[] nums) {
+import java.util.HashSet;
+import java.util.Set;
+
+class LongestConsecutive {
+    public static void main(String[] args) {
+        int[] nums = {0,3,7,2,5,8,4,6,0,1};
+        System.out.println(longestConsecutive(nums));
+    }
+    public static int longestConsecutive(int[] nums) {
         Set<Integer> numSet = new HashSet<>();
         for(int n: nums) {
             numSet.add(n);
@@ -9,9 +16,9 @@ class Solution {
         int maxLen = 1;
         for(int i = 0 ; i < nums.length; i++) {
             if(!numSet.contains(nums[i] - 1)) {
-                maxLen = 1;
+                maxLen = Math.max(maxLen, 1);
                 int k = i;
-                while(k < nums.length &&numSet.contains(nums[k])) {
+                while(k < nums.length &&numSet.contains(nums[k])&& nums) {
                     maxLen++;
                     k++;
                 }
