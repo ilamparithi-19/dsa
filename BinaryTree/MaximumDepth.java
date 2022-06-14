a263e8460ebce036cc6f54883d83c1b2d975ae87
Ela <androelam@gmail.com>
2022-06-14 22:33:50 +0530

    max depth

---
+++ BinaryTree/MaximumDepth.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class MaximumDepth {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1 + Math.max(lh, rh);
+    }
+}
