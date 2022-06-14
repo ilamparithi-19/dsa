ee7d46517308174098c4fa638fa7df72ac3ebc7d
Ela <androelam@gmail.com>
2022-06-14 22:38:28 +0530

    updated code

--- BinaryTree/MaximumDepth.java
+++ BinaryTree/MaximumDepth.java
@@ -1,12 +1,27 @@
 package BinaryTree;
 
 public class MaximumDepth {
-    public int maxDepth(TreeNode root) {
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedUtil(root) != -1;
+    }
+    
+    private int isBalancedUtil(TreeNode root) {
         if(root == null) {
             return 0;
         }
-        int lh = maxDepth(root.left);
-        int rh = maxDepth(root.right);
+        
+        int lh = isBalancedUtil(root.left);
+        if(lh == -1) {
+            return lh;
+        }
+        
+        int rh = isBalancedUtil(root.right);
+        if(rh == -1) {
+            return rh;
+        }
+        if(Math.abs(lh-rh) > 1) {
+            return -1;
+        }
         return 1 + Math.max(lh, rh);
     }
 }
