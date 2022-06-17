2deb851df7f113b5b4069204e5739214bd972984
Ela <androelam@gmail.com>
2022-06-17 23:04:58 +0530

    bfs

--- Graph/bfs.java
+++ Graph/bfs.java
@@ -1,47 +1,11 @@
-package Graph;
-
-// { Driver Code Starts
-// Initial Template for Java
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj =
-                new ArrayList<ArrayList<Integer>>();
-            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
-            for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
-            for (int i = 0; i < ans.size(); i++)
-                System.out.print(ans.get(i) + " ");
-            System.out.println();
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to return a list containing the DFS traversal of the graph.
-    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
+         // Code here
         boolean[] vis = new boolean[V+1];
         ArrayList<Integer> bfs = new ArrayList<>();
-        for(int i = 0; i <= V; i++) {
+        for(int i = 0; i < V; i++) {
             if(!vis[i]) {
                  Queue<Integer> ds = new LinkedList<>();
                  ds.add(i);
@@ -49,7 +13,7 @@ class Solution {
                  while(!ds.isEmpty()) {
                      Integer node = ds.poll();
                      bfs.add(node);
-                     for(int adjEle: adj.get(i)) {
+                     for(Integer adjEle: adj.get(node)) {
                          if(!vis[adjEle]) {
                              vis[adjEle] = true;
                              ds.add(adjEle);
