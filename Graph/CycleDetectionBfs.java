9a2c6118341aacb1a80de0b807de7f8cbcda539d
Elambaruthi <59686984+androelam@users.noreply.github.com>
2022-07-28 08:43:21 +0530

    cycle detection BFS

--- Graph/CycleDetectionBfs.java
+++ Graph/CycleDetectionBfs.java
@@ -15,33 +15,33 @@ class Node {
 }
 class Solution {
     // Function to return Breadth First Traversal of given graph.
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-         // Code here
-        boolean[] vis = new boolean[V+1];
-        ArrayList<Integer> bfs = new ArrayList<>();
-        for(int i = 0; i < V; i++) {
-            if(!vis[i]) {
-                 Queue<Integer> ds = new LinkedList<>();
-                 ds.add(i);
-                 vis[i] = true;
-                 while(!ds.isEmpty()) {
-                     Integer node = ds.poll();
-                     bfs.add(node);
-                     for(Integer adjEle: adj.get(node)) {
-                         if(!vis[adjEle]) {
-                             vis[adjEle] = true;
-                             ds.add(adjEle);
-                         }
-                     }
-                 }
-            }
-        }
-        return bfs;
-    }
+    // public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+    //     // Code here
+    //      // Code here
+    //     boolean[] vis = new boolean[V+1];
+    //     ArrayList<Integer> bfs = new ArrayList<>();
+    //     for(int i = 0; i < V; i++) {
+    //         if(!vis[i]) {
+    //              Queue<Integer> ds = new LinkedList<>();
+    //              ds.add(i);
+    //              vis[i] = true;
+    //              while(!ds.isEmpty()) {
+    //                  Integer node = ds.poll();
+    //                  bfs.add(node);
+    //                  for(Integer adjEle: adj.get(node)) {
+    //                      if(!vis[adjEle]) {
+    //                          vis[adjEle] = true;
+    //                          ds.add(adjEle);
+    //                      }
+    //                  }
+    //              }
+    //         }
+    //     }
+    //     return bfs;
+    // }
     private static boolean checkForCycle(int s, boolean vis[], ArrayList<ArrayList<Integer>> adj) {
         Queue<Node> bfsQ = new LinkedList<>();
-
+        vis[s] = true;
         bfsQ.add(new Node(s, -1));
         while(!bfsQ.isEmpty()) {
             int node = bfsQ.peek().first;
