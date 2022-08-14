8337215f5f165778bed4ebdd6ea63c706052f2e5
elambaruthi+1-at-192223248060 <ela@raddy.io>
2022-08-14 23:09:41 +0530

    Updated

--- BinaryTree/MaximumDepth.java
+++ BinaryTree/MaximumDepth.java
@@ -24,4 +24,17 @@ public class MaximumDepth {
         }
         return 1 + Math.max(lh, rh);
     }
+
 }
+
+/*
+ *  public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = 1 + maxDepth(root.left);
+        int rh = 1 + maxDepth(root.right);
+        return Math.max(lh, rh);
+    }
+ */
+//BFS - Level Order traversal also another solution
