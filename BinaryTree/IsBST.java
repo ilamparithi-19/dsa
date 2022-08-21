9925edfd691d1bdf1f39ec4ee346330c01bd38dd
Elambaruthi <59686984+androelam@users.noreply.github.com>
2022-08-21 23:01:31 +0530

    reafactoring

---
+++ BinaryTree/IsBST.java
@@ -0,0 +1,39 @@
+package BinaryTree;
+
+public class IsBST {
+    class TreeNode
+{
+    int data;
+    Node left, right;
+ 
+    public TreeNode(int data)
+    {
+        this.data = data;
+        left = right = null;
+    }
+}
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedUtil(root) != -1;
+    }
+    
+    private int isBalancedUtil(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        
+        int lh = isBalancedUtil(root.left);
+        if(lh == -1) {
+            return lh;
+        }
+        
+        int rh = isBalancedUtil(root.right);
+        if(rh == -1) {
+            return rh;
+        }
+        if(Math.abs(lh-rh) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(lh, rh);
+    }
+
+}
