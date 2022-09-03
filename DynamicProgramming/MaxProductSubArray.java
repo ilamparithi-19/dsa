a0f4ad4a87794c8b7e2ca261cf4946c962a02bb2
elambaruthi+1-at-192223248060 <ela@raddy.io>
2022-09-03 22:46:52 +0530

    max product sub array kadane

---
+++ DynamicProgramming/MaxProductSubArray.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+import java.util.*;
+
+public class MaxProductSubArray
+{
+	static int maxProductSubArray(int arr[]) {
+    int prod1 = arr[0],prod2 = arr[0],result = arr[0];
+    
+    for(int i=1;i<arr.length;i++) {
+        int temp = Math.max(arr[i],Math.max(prod1*arr[i],prod2*arr[i]));
+        prod2 = Math.min(arr[i],Math.min(prod1*arr[i],prod2*arr[i]));
+        prod1 = temp;
+        
+        result = Math.max(result,prod1);
+    }
+    
+    return result;
+	}
+	public static void main(String[] args) {
+		int nums[] = {1,2,-3,0,-4,-5};
+		int answer = maxProductSubArray(nums);
+		System.out.print("The maximum product subarray is: "+answer);
+	}
+}
